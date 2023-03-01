934ee734d98852a28f4897c18c9780e5c8823b8a
Kyla <lekan.sofuyi01@gmail.com>
2023-03-01 17:09:17 +0100

    Swagger Removed from Project

--- src/main/java/africa/vote/SmartVote/security/applicationconfig/ApplicationConfig.java
+++ src/main/java/africa/vote/SmartVote/security/applicationconfig/ApplicationConfig.java
@@ -13,16 +13,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.*;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
-
-import java.util.Collections;
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -52,46 +42,4 @@ public class ApplicationConfig {
     public PasswordEncoder passwordEncoder(){ return new BCryptPasswordEncoder();}
     @Bean
     public ModelMapper modelMapper(){return new ModelMapper();}
-
-    private static final String AUTHORIZATION_HEADER = "Authorization";
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .securitySchemes(Collections.singletonList(apiKey()))
-                .securityContexts(Collections.singletonList(securityContext()))
-                .select()
-                .paths(PathSelectors.any())
-                .apis(RequestHandlerSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Smart Voting System")
-                .description("API Reference for Smart Voting System ")
-                .version("1.0")
-                .contact(new Contact("Olalekan Sofuyi",
-                        "www.github.com/olalekan-01",
-                        "lekan@gmail.com"))
-                .build();
-    }
-
-    private SecurityContext securityContext() {
-        return SecurityContext.builder()
-                .securityReferences(defaultAuth())
-                .build();
-    }
-
-    private ApiKey apiKey() {
-        return new ApiKey(AUTHORIZATION_HEADER, "JWT", "header");
-    }
-
-    private List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope
-                = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return List.of(new SecurityReference("JWT", authorizationScopes));
-    }
 }
